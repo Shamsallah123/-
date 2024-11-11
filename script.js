83be174f190b1119b52a9524ac7746449c088734
shams <sobmanlalay@gmail.com>
2024-11-11 23:34:41 +0430

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("ستاسو پیغام په بریالیتوب سره واستول شو. مننه!");
+    this.reset();
+});
